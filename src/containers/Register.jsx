dbe85d198fb5c940379e5982f7fc6a3714df7d74
matiaswasiak <matiaswasiak@gmail.com>
2021-02-22 18:02:12 -0300

    Manejando enlaces y configuraciones

--- src/containers/Register.jsx
+++ src/containers/Register.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "../assets/styles/components/Register.scss";
 
 const Register = () => (
@@ -13,7 +14,7 @@ const Register = () => (
           Registrarme
         </button>
       </form>
-      <a href="/">Iniciar sesión</a>
+      <Link to="/login">Iniciar sesión</Link>
     </section>
   </section>
 );
